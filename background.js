4f5c4356304172e2eff2749b07d30384646c3861
matheushalas <62209902+matheushalas@users.noreply.github.com>
2020-09-15 11:03:31 -0300

    Add files via upload

--- background.js
+++ background.js
@@ -1,6 +1,14 @@
+//filtro de url para bloquear
+defaultFilters = [
+    "*://*.reddit.com/*",
+    "*://*.youtube.com/*",
+    "*://*.wallpapershome.com/*",
+    "*://*.tpc.googlesyndication.com/daca_images/simgad/14665391136553537509/*",
+] 
+
 chrome.webRequest.onBeforeRequest.addListener(
     function(details) { return { cancel: true }},
-    { urls: []},
+    { urls: defaultFilters },
     ["blocking"]
 )
 
